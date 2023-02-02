5ba33017cad788d7b3e1c08b318c1f03b057216d
ChauNguyet98 <chaunguyet1998@gmail.com>
2023-02-02 19:58:42 +0700

    middleware

--- src/server.js
+++ src/server.js
@@ -19,10 +19,19 @@ import initApiRoute from "./route/api";
 // import connection from "./configs/connectDB";
 
 require("dotenv").config();
+var morgan = require("morgan");
 
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  console.log(">> check");
+  console.log(req.method);
+  next();
+});
+
+app.use(morgan("combined"));
 
 // configure to send data from client to server
 app.use(express.urlencoded({ extended: true }));
@@ -31,6 +40,11 @@ app.use(express.json());
 configViewEngine(app);
 initWebRoute(app);
 initApiRoute(app);
+
+//handle 404 not found
+app.use((req, res) => {
+  return res.render("404-not-found.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
